8eb096b5f76dd2fa8f4cf21d056c9306ebc9558f
mlazarte <martinlazarte@alu.frt.utn.edu.ar>
2021-03-13 01:53:30 -0300

    Modificada App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      name: "Hola soy martin aprendiendo react!",
+      age: 26
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{this.state.name} y mi edad es {this.state.age}</p>
+        <button onClick = {() => this.setState({ name: "Hola soy martin apretando un boton", age: 27})}>Press Me!</button>
       </header>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
